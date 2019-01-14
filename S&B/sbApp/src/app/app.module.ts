95f5b30b1e852704879c788ff404aa071f535317
NickDiDomenico <DiDomenico@users.noreply.github.com>
2019-01-14 10:10:35 -0500

    Components

--- S&B/sbApp/src/app/app.module.ts
+++ S&B/sbApp/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { HomeComponent } from './home/home.component';
+import { CustomerComponent } from './customer/customer.component';
+import { LoginComponent } from './Components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    HomeComponent,
+    CustomerComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
