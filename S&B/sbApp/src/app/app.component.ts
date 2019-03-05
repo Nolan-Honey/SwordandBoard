26923b7e54dcc4242160fc336b91cc5729e8e0cc
NickDiDomenico <DiDomenico@users.noreply.github.com>
2019-03-05 12:31:02 -0500

    background effects

--- S&B/sbApp/src/app/app.component.ts
+++ S&B/sbApp/src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-
+import { Component, Renderer2 } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +6,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'App Component';
+  toState = 'state1';
+  constructor(private renderer: Renderer2) {}
+
+  bg1() {
+    this.renderer.setStyle(document.body, 'background-image', 'url("../assets/images/backgrounds/bg.jpg")');
+  }
+  bg2() {
+    this.renderer.setStyle(document.body, 'background-image', 'url("../assets/images/backgrounds/bg2.jpg")');
+  }
+  bg3() {
+    this.renderer.setStyle(document.body, 'background-image', 'url("../assets/images/backgrounds/bg5.jpg")');
+  }
+  bg4() {
+    this.renderer.setStyle(document.body, 'background-image', 'url("../assets/images/backgrounds/bg3-invert.jpg")');
+  }
+  bg5() {
+    this.renderer.setStyle(document.body, 'background-image', 'url("../assets/images/backgrounds/bg6-invert.jpg")');
+  }
 }
