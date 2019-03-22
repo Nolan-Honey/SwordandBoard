97a60a4f0e7cf584c81e1f083e6fb6af9651cc54
saif <sfmustafa2@gmail.comngh>
2019-03-22 00:53:44 -0400

    customer info sync to customer profile

--- S&B/sbApp/src/app/Components/Customers/customer/customer.component.ts
+++ S&B/sbApp/src/app/Components/Customers/customer/customer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {formatDate} from '@angular/common';
 import { Subscription } from 'rxjs';
 import { AuthService } from 'src/app/Services/auth.service';
 import { CustomerService } from '../../../Services/customer.service';
@@ -13,7 +14,8 @@ export class CustomerComponent implements OnInit {
   title = 'My Profile';
   userIsAuthenticated = false;
   customerLoaded = false;
-  private customer: any
+  now = formatDate(new Date(), 'yyyy/MM/dd', 'en');;
+  private customer: any;
   private authListenerSubs: Subscription;
   constructor(
     private authService: AuthService,
@@ -28,9 +30,12 @@ export class CustomerComponent implements OnInit {
         this.userIsAuthenticated = isAuthenticated;
       });
 
+    let currentDate = new Date();
+    
     this.customerService.viewCustomer(localStorage.getItem("currentUserId")).subscribe(res => {
       this.customer = res
       this.customerLoaded = true
+      console.log(this.now)
     })
     
   }
