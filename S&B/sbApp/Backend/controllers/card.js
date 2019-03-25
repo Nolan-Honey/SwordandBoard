bf0b3d2acce382996a64f9a352aeb4613840663c
JanFarun <35471426+JanFarun@users.noreply.github.com>
2019-03-25 19:46:15 -0400

    servers run but cant login

--- S&B/sbApp/Backend/controllers/card.js
+++ S&B/sbApp/Backend/controllers/card.js
@@ -2,12 +2,32 @@ const mongoose = require('mongoose');
 
 module.exports.card = (req, res, next) => {
 var cardValue = req.body.cardName;
+var white = req.body.white
+var blue = req.body.blue
+var black = req.body.black
+var red = req.body.red
+var green = req.body.green
+var colors =[]
+if(white == true){
+    white = "W"
+    if(blue == true){
+        blue = "B"
+        if(black == true){
+            black = "U"
+            if(red == true){
+                red = "R"
+                if(green == true){
+                    green = "G"
+                }
+            }
+        }
+    }
+}
 var card = mongoose.model('Scryfall');
 card.find({name: { "$regex": cardValue, "$options": "i" } },function (err, docs) {
     if(err){
-        
     }
-    console.log(docs)
+    console.log(white,blue,black,red,green)
     res.send(docs)
 })
 }
--- S&B/sbApp/src/app/Components/home/home.component.ts
+++ S&B/sbApp/src/app/Components/home/home.component.ts
@@ -6,11 +6,13 @@ import { NgxSpinnerService } from 'ngx-spinner';
 import { Response } from 'selenium-webdriver/http';
 import { AuthService } from 'src/app/Services/auth.service';
 import { Subscription } from 'rxjs';
+import { networkInterfaces } from 'os';
 
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
@@ -44,13 +46,20 @@ export class HomeComponent implements OnInit {
   }
   //^^^^^^^^^^^^^^^^^^^^^^Search Cards^^^^^^^^^^^^^^^^^^^
   newCard = new FormGroup({
-    cardName: new FormControl('')
+    cardName: new FormControl(''),
+    white: new FormControl(''),
+    blue: new FormControl(''),
+    black: new FormControl(''),
+    red: new FormControl(''),
+    green: new FormControl('')
   })
 
   onSubmit() {
+    console.log(this.newCard.value)
     this.spinner.show()
-    this.cardName = this.newCard.get('cardName').value;
-    this.cardInfoService.viewCard(this.newCard.value).subscribe(
+    // this.cardName = this.newCard.get('cardName').value;
+    //this.cardInfoService.viewCard(this.newCard.value).subscribe(
+      this.cardInfoService.viewCard(this.newCard.value).subscribe(
       res => {
         this.cards = res;
         this.spinner.hide()
