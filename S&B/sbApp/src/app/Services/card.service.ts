4924a6d2a2a46653a57af777bd00de2b94be135b
JanFarun <35471426+JanFarun@users.noreply.github.com>
2019-03-27 21:53:26 -0400

    Search fix for sets , no diffrent sets and card flip

--- S&B/sbApp/src/app/Services/card.service.ts
+++ S&B/sbApp/src/app/Services/card.service.ts
@@ -1,11 +1,14 @@
 import {Injectable} from '@angular/core';
 import {CardInfo} from '../Shared/cardInfo.model';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { environment } from '../../environments/environment'
 import { Observable } from 'rxjs/observable'
 import {map} from 'rxjs/operators'
 import { delay } from 'q';
 import { Params } from '@angular/router';
+import 'rxjs/add/operator/catch'
+import 'rxjs/add/observable/throw'
+import { observable } from 'rxjs';
 @Injectable({
     providedIn:'root'
 })
@@ -22,9 +25,12 @@ export class cardService{
 
     getcardURL = environment.apiBaseUrl+'/card';
     viewCard(card:String){
-        return this.http.post(this.getcardURL,card);
+        return this.http.post(this.getcardURL,card).catch(this.errorHandler);
     }
     getCards(card:Params){
         return this.http.get(this.getcardURL,card);
+    }
+    errorHandler(error:HttpErrorResponse){
+        return Observable.throw(error.message ||"Server Error")
     }
 }
